eb970ae63327ea8cd38b2598d7f8e4b230578dd6
Hertzole <hampusringstrom@gmail.com>
2023-05-22 22:32:35 +0200

    add: async extensions to stats

---
+++ Packages/se.hertzole.small-steamworks/Runtime/Extensions/StatsExtensionsAsync.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hertzole.SmallSteamworks
+{
+	public static partial class SteamExtensions
+	{
+		public static Task<UserStatsReceivedResponse> RequestCurrentStatsAsync(this ISteamStats stats, CancellationToken cancellationToken = default)
+		{
+			TaskCompletionSource<UserStatsReceivedResponse> tcs = new TaskCompletionSource<UserStatsReceivedResponse>();
+
+			stats.RequestCurrentStats((success, steamUserStats) =>
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					tcs.SetCanceled();
+					return;
+				}
+
+				tcs.SetResult(new UserStatsReceivedResponse(success, steamUserStats));
+			});
+
+			return tcs.Task;
+		}
+
+		public static Task<UserStatsReceivedResponse> RequestUserStatsAsync(this ISteamStats stats,
+			in SteamID steamId,
+			CancellationToken cancellationToken = default)
+		{
+			TaskCompletionSource<UserStatsReceivedResponse> tcs = new TaskCompletionSource<UserStatsReceivedResponse>();
+
+			stats.RequestUserStats(steamId, (success, steamUserStats) =>
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					tcs.SetCanceled();
+					return;
+				}
+
+				tcs.SetResult(new UserStatsReceivedResponse(success, steamUserStats));
+			});
+
+			return tcs.Task;
+		}
+	}
+}
