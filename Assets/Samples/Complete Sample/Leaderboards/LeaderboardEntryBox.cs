6d61c95e445ab59ceba9bf47e027482901bd9cc6
Hertzole <hampusringstrom@gmail.com>
2023-05-22 22:51:13 +0200

    add: leaderboard sample

---
+++ Assets/Samples/Complete Sample/Leaderboards/LeaderboardEntryBox.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hertzole.SmallSteamworks.CompleteSample
+{
+	public class LeaderboardEntryBox : MonoBehaviour
+	{
+		[SerializeField]
+		private TMP_Text rankLabel = default;
+		[SerializeField]
+		private RawImage avatar = default;
+		[SerializeField]
+		private TMP_Text nameLabel = default;
+		[SerializeField]
+		private TMP_Text scoreLabel = default;
+
+		private SteamImage? avatarImage;
+
+		private void OnDestroy()
+		{
+			if (avatarImage != null)
+			{
+				avatarImage.Value.Dispose();
+				avatarImage = null;
+			}
+		}
+
+		public void SetEntry(SteamLeaderboardEntry entry)
+		{
+			if (avatarImage != null)
+			{
+				avatarImage.Value.Dispose();
+				avatarImage = null;
+			}
+
+			rankLabel.text = $"#{entry.GlobalRank}";
+			scoreLabel.text = entry.Score.ToString(CultureInfo.InvariantCulture);
+			nameLabel.text = entry.UserID.Name;
+
+			SteamManager.Friends.GetAvatar(entry.UserID.SteamID, AvatarSize.Large, (image, _, _, _) =>
+			{
+				avatarImage = image;
+				avatar.texture = image.Texture;
+			});
+		}
+	}
+}
