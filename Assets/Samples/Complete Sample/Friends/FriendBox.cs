9e6c72b48f11707bd63663fe5a987175884a2b63
Hertzole <hampusringstrom@gmail.com>
2023-05-26 17:12:34 +0200

    add: friends sample

---
+++ Assets/Samples/Complete Sample/Friends/FriendBox.cs
@@ -0,0 +1,63 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hertzole.SmallSteamworks.CompleteSample
+{
+	public class FriendBox : MonoBehaviour
+	{
+		[SerializeField]
+		private bool isMe = default;
+
+		[Space]
+		[SerializeField]
+		private RawImage icon = default;
+		[SerializeField]
+		private TMP_Text nameLabel = default;
+		[SerializeField]
+		private TMP_Text statusLabel = default;
+
+		private SteamImage avatarImage;
+
+		private void Start()
+		{
+			if (isMe)
+			{
+				SetUser(SteamManager.Friends.Me);
+			}
+		}
+
+		private void OnDestroy()
+		{
+			ClearAvatar();
+		}
+
+		public void SetUser(SteamUser user)
+		{
+			nameLabel.text = user.Name;
+
+			if (statusLabel != null)
+			{
+				statusLabel.text = user.IsOnline ? "Online" : "Offline";
+			}
+
+			icon.color = user.IsOnline ? Color.white : new Color(1, 1, 1, 0.1f);
+
+			ClearAvatar();
+
+			SteamManager.Friends.GetAvatar(user.SteamID, AvatarSize.Large, (image, id, width, height) =>
+			{
+				avatarImage = image;
+				icon.texture = image.Texture;
+			});
+		}
+
+		private void ClearAvatar()
+		{
+			if (avatarImage.IsValid)
+			{
+				avatarImage.Dispose();
+			}
+		}
+	}
+}
--- Assets/Samples/Complete Sample/Friends/FriendsSample.cs
+++ Assets/Samples/Complete Sample/Friends/FriendsSample.cs
@@ -1,9 +1,62 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Hertzole.SmallSteamworks.Helpers;
+using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Hertzole.SmallSteamworks.CompleteSample
 {
 	public class FriendsSample : BaseSample
 	{
-		
+		[SerializeField]
+		private FriendBox friendBoxPrefab = default;
+		[SerializeField]
+		private RectTransform friendsContainer = default;
+
+		private bool isGettingFriends;
+
+		private readonly List<FriendBox> entryBoxes = new List<FriendBox>();
+
+		private ObjectPool<FriendBox> entryBoxPool;
+
+		private void Awake()
+		{
+			entryBoxPool = new ObjectPool<FriendBox>(
+				() => Instantiate(friendBoxPrefab, friendsContainer), // Create
+				b =>
+				{
+					b.gameObject.SetActive(true);
+					b.transform.SetAsLastSibling();
+				}, // On Get
+				b => b.gameObject.SetActive(false)); // On Release
+		}
+
+		public override void ShowSample()
+		{
+			if (isGettingFriends)
+			{
+				return;
+			}
+
+			isGettingFriends = true;
+
+			LoadFriends();
+		}
+
+		private void LoadFriends()
+		{
+			for (int i = 0; i < entryBoxes.Count; i++)
+			{
+				entryBoxPool.Release(entryBoxes[i]);
+			}
+
+			entryBoxes.Clear();
+
+			foreach (SteamUser friend in SteamManager.Friends.GetFriends())
+			{
+				FriendBox box = entryBoxPool.Get();
+				box.SetUser(friend);
+				entryBoxes.Add(box);
+			}
+		}
 	}
 }
