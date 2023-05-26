d40129e874b901ea93fe1dc3af8ecccedeb502cc
Hertzole <hampusringstrom@gmail.com>
2023-05-26 18:47:21 +0200

    add: github workflows

---
+++ Assets/Editor/CreateSolution.cs
@@ -0,0 +1,17 @@
+#if UNITY_EDITOR
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEditor;
+
+namespace GitTools
+{
+	public static class Solution
+	{
+		public static void Sync()
+		{
+			ProjectGeneration projectGeneration = new ProjectGeneration();
+			AssetDatabase.Refresh();
+			projectGeneration.GenerateAndWriteSolutionAndProjects();
+		}
+	}
+}
+#endif
