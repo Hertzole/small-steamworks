d9b045df3d53ab46f678f9daaf383713eb664b22
Hertzole <hampusringstrom@gmail.com>
2023-05-30 00:02:33 +0200

    fix: cleaned up project

--- Runtime/Storage/SteamWriteBatch.cs
+++ Runtime/Storage/SteamWriteBatch.cs
@@ -5,8 +5,33 @@ using Steamworks;
 
 namespace Hertzole.SmallSteamworks
 {
-	public readonly struct SteamWriteBatch : IDisposable
+	public readonly struct SteamWriteBatch : IDisposable, IEquatable<SteamWriteBatch>
 	{
+		public bool Equals(SteamWriteBatch other)
+		{
+			return true;
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is SteamWriteBatch other && Equals(other);
+		}
+
+		public override int GetHashCode()
+		{
+			return 0;
+		}
+
+		public static bool operator ==(SteamWriteBatch left, SteamWriteBatch right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(SteamWriteBatch left, SteamWriteBatch right)
+		{
+			return !left.Equals(right);
+		}
+
 		public void Dispose()
 		{
 #if !DISABLESTEAMWORKS
