9bb31a720ff014b0087c8ac00ea996bc26c432e0
Hertzole <hampusringstrom@gmail.com>
2023-05-30 14:13:46 +0200

    add: drawers for AppId and SteamId

---
+++ Editor/Drawers/AppIdDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Hertzole.SmallSteamworks.Editor
+{
+	[CustomPropertyDrawer(typeof(AppId))]
+	public sealed class AppIdDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			SerializedProperty valueProperty = property.FindPropertyRelative("value");
+			UIntField.DrawGUI(position, valueProperty, label);
+		}
+
+		public override VisualElement CreatePropertyGUI(SerializedProperty property)
+		{
+			string label =
+#if UNITY_2022_2_OR_NEWER
+				preferredLabel;
+#else
+				property.displayName;
+#endif
+
+			VisualElement field = UIntField.CreatePropertyGUI(property.FindPropertyRelative("value"), label);
+			field.AddToClassList(BaseField<object>.alignedFieldUssClassName);
+
+			return field;
+		}
+	}
+}
---
+++ Editor/Drawers/SteamIdDrawer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Hertzole.SmallSteamworks.Editor
+{
+	[CustomPropertyDrawer(typeof(SteamID))]
+	public sealed class SteamIdDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			SerializedProperty valueProperty = property.FindPropertyRelative("value");
+			ULongField.DrawGUI(position, valueProperty, label);
+		}
+
+		public override VisualElement CreatePropertyGUI(SerializedProperty property)
+		{
+			string label =
+#if UNITY_2022_2_OR_NEWER
+				preferredLabel;
+#else
+				property.displayName;
+#endif
+
+			VisualElement field = ULongField.CreatePropertyGUI(property.FindPropertyRelative("value"), label);
+			field.AddToClassList(BaseField<object>.alignedFieldUssClassName);
+
+			return field;
+		}
+	}
+}
