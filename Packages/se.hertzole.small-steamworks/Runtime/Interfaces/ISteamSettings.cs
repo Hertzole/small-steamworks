fffbd585b6e1ba36d19dc9a8aa1ec43b37136e28
Hertzole <hampusringstrom@gmail.com>
2023-05-31 13:02:31 +0200

    add: documentation for settings

--- Packages/se.hertzole.small-steamworks/Runtime/Interfaces/ISteamSettings.cs
+++ Packages/se.hertzole.small-steamworks/Runtime/Interfaces/ISteamSettings.cs
@@ -2,10 +2,19 @@
 {
 	public interface ISteamSettings
 	{
+		/// <summary>
+		///     The AppID of the game.
+		/// </summary>
 		AppID AppID { get; set; }
-		
+
+		/// <summary>
+		///     If true, checks if your executable was launched through Steam and relaunches it through Steam if it wasn't.
+		/// </summary>
 		bool RestartAppIfNecessary { get; set; }
-		
+
+		/// <summary>
+		///     If true, the current user's stats will be fetched when the game starts.
+		/// </summary>
 		bool FetchCurrentStatsOnBoot { get; set; }
 	}
 }
